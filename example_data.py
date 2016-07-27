79dd7de0030d3cca52226aafb53c680e8f3eb6f4
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-07-27 14:43:56 +0200

    Bug fixed for dictionary

--- example_data.py
+++ example_data.py
@@ -3,34 +3,19 @@
 from models import *
 from peewee import *
 
-city_attribute = ["applicant_city", "school_city"]
-applicant_attribute = ["first_name", "last_name", "school", "code", "city",  "status"]
-
-
-city_rows = [["Eger", "Miskolc"],
-             ["Balassagyarmat", "Budapest"],
-             ["Vac", "Budapest"],
-             ["Budapest", "Budapest"],
-             ["Ostrava", "Krakow"]
-             ]
-
-applicant_rows = [["Janos", "Kiss", None, None, "Eger", "new"],
-                  ["Anna", "Nagy", None, None, "Vac",  "new"],
-                  ["Joci", "Galambos", None, None, "Budapest", "new"],
-                  ["Ágnes", "Jakabos", None, None, "Balassagyarmat", "new"]
+applicant_dict = [{"first_name": "Janos", "last_name": "Kiss", "city": "Eger"},
+                  {"first_name": "Ági", "last_name": "Kincses", "city": "Budapest"},
+                  {"first_name": "Görbe", "last_name": "Barna", "city": "Szentendre"},
+                  {"first_name": "Virág", "last_name": "Ibolya", "city": "Budapest"}
                   ]
 
-city_dict = []
-applicant_dict = []
-
-for i in len(city_rows):
-    city_dict.append(dict(zip(city_attribute, city_rows[i])))
-
-for j in len(applicant_dict):
-    applicant_dict.append(dict(zip(applicant_attribute, applicant_rows[i])))
-
+city_dict = [{"applicant_city": "Szentendre", "school": "Budapest"},
+             {"applicant_city": "Eger", "school": "Miskolc"},
+             {"applicant_city": "Ostrava", "school": "Warsaw"},
+             {"applicant_city": "Budapest", "school": "Budapest"}
+             ]
 
 # insert in a fast way
 with db.atomic():
     City.insert_many(city_dict).execute()
-    Applicant_dict.insert_many(applicant_dict).execute()
+    Applicant.insert_many(applicant_dict).execute()

9046b049a84939e9f9479cc81af472602f27d4b6
b-franka <b.franku@gmail.com>
2016-08-23 13:39:04 +0200

    bugfix

--- emails.py
+++ emails.py
@@ -1,7 +1,7 @@
 class Email():
 
-    @classmethod
-    def send_email(cls, recipient, user, pwd, subject, body):
+    @staticmethod
+    def send_email(recipient, user, pwd, subject, body):
         import smtplib
 
         gmail_user = user
@@ -24,35 +24,3 @@ class Email():
             print('successfully sent the mail')
         except:
             print("failed to send mail")
-
-#     @staticmethod
-#     def test_email():
-#         import smtplib
-#
-#         user = "girhes.cc.2016@gmail.com"
-#         pwd = "Girhes2016"
-#         subject = "Testing 1, 2, 3..."
-#         body = "This is an automatically generated e-mail to test the mail service."
-#
-#         gmail_user = user
-#         gmail_pwd = pwd
-#         FROM = user
-#         TO = [user]
-#         SUBJECT = subject
-#         TEXT = body
-#
-#         # Prepare actual message
-#         message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-#         """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-#         try:
-#             server = smtplib.SMTP("smtp.gmail.com", 587)
-#             server.ehlo()
-#             server.starttls()
-#             server.login(gmail_user, gmail_pwd)
-#             server.sendmail(FROM, TO, message)
-#             server.close()
-#             print('successfully sent the mail')
-#         except:
-#             print("failed to send mail")
-#
-# Email.test_email()

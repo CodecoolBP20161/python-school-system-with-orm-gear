33c21259b9d45fd0b699639ba576db181ac916f6
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-08-23 14:37:29 +0200

    message for interview applicant ready

--- message.py
+++ message.py
@@ -4,8 +4,11 @@ class Message:
     def new_applicant(cls, first_name, code, school):
         subject = "Codecool application process"
         message = """Dear {0},
+
                      Your application code is: {1}
-                     Your school is: {2} """.format(first_name, code, school)
+                     Your school is: {2}
+                     Best Regards,
+                     CodeCool   """.format(first_name, code, school)
 
         message_dict = {"subject": subject,
                         "body": message
@@ -14,5 +17,17 @@ class Message:
         return message_dict
 
     @classmethod
-    def apllicant_interview(cls):
-        pass
+    def apllicant_interview(cls, first_name, time, mentor_first_name, mentor_last_name):
+        subject = "Codecool interview information"
+        message = """Dear {0},
+
+                     Your interview time slot is: {1}
+                     Your mentor is: {2} {3}
+                     Best Regards,
+                     Codecool   """.format(first_name, time, mentor_first_name, mentor_last_name)
+
+        message_dict = {"subject": subject,
+                        "body": message
+                        }
+
+        return message_dict

cc418ead2a88f3a693023bd9ed285d19a710f759
Milu László <hockeylaci@gmail.com>
2016-09-06 13:49:54 +0200

    fixed bug that after registration continuous refresh would submit the form again and again

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import *
 from models import *
-
+from validation import Validation
+import os
 app = Flask(__name__)
 db.connect()
 
-app = Flask(__name__)
 
-HOST = 'http://localhost:5000/'
+
+# HOST = 'http://localhost:5000/'
+
+secret = os.urandom(24)
+DEBUG = True
+app.secret_key = secret
+USERNAME = 'admin'
+PASSWORD = 'default'
 
 @app.route('/')
 def index():
@@ -15,15 +22,20 @@ def index():
 @app.route('/registration', methods=['GET'])
 def registration_form():
     empty_object = Applicant(first_name="", last_name="", email="", city="")
-    return render_template('registration.html', applicant = empty_object)
+    return render_template('registration.html', applicant=empty_object)
 
 @app.route('/registration', methods=['POST'])
 def register():
+    # first_name = request.form['first_name']
+    # last_name = request.form['last_name']
+    # email = request.form['email']
+
     data = Applicant.create(first_name=request.form['first_name'],
                             last_name=request.form['last_name'],
                             email=request.form['email'],
                             city=request.form['city'])
-    return 'Registration sent.'
+    flash('Thanks for your registration')
+    return redirect('/registration')
 
 
 if __name__ == '__main__':

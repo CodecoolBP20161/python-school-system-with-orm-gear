972af3a7d5c29410b484e3bbebe93de5958079cb
Milu László <hockeylaci@gmail.com>
2016-09-06 13:15:28 +0200

    /registration web page is ready without validation

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-from flask import Flask
+from flask import *
+from models import *
+
+app = Flask(__name__)
+db.connect()
+
+app = Flask(__name__)
+
+HOST = 'http://localhost:5000/'
+
+@app.route('/')
+def index():
+    return 'Hello flask!'
+
+@app.route('/registration', methods=['GET'])
+def registration_form():
+    empty_object = Applicant(first_name="", last_name="", email="", city="")
+    return render_template('registration.html', applicant = empty_object)
+
+@app.route('/registration', methods=['POST'])
+def register():
+    data = Applicant.create(first_name=request.form['first_name'],
+                            last_name=request.form['last_name'],
+                            email=request.form['email'],
+                            city=request.form['city'])
+    return 'Registration sent.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

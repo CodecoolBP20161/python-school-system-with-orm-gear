1c2a5d07bac84ac17d383bdb480da19813a22f8b
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-09-07 13:18:20 +0200

    login works with decorator

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import *
 from models import *
+import json
 from validation import Validation
+from functools import *
 import os
 app = Flask(__name__)
 db.connect()
@@ -13,11 +15,21 @@ app.secret_key = secret
 USERNAME = 'admin'
 PASSWORD = 'default'
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('username') is None:
+            return redirect(url_for('index', next=request.url))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/registration', methods=['GET', 'POST'])
+
 def registration_form():
     try:
         applicant = Applicant.create_from_form(request.form)
@@ -36,9 +48,37 @@ def registration_form():
                 flash(values)
     return render_template('registration.html', applicant=applicant)
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    with open('admin.json') as json_data:
+        admin_data = json.load(json_data)
+        print(admin_data['username'], "json")
+    if request.method == "POST":
+        print(request.form['username'], "post")
+        if request.form['username'] != admin_data['username']:
+            flash('Wrong user name')
+            return render_template('login.html')
+        elif request.form['password'] != admin_data['password']:
+            flash('Wrong passworld')
+            return render_template('login.html')
+        else:
+            session['username'] = request.form['username']
+            flash("You have logged in. Welcome on the board")
+
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    # flash('You were logged out')
+    return 'You were logged out'
+
 
 
 @app.route('/admin/e-mail-log')
+@login_required
 def email_log():
     email = Email_log.select()
     return render_template('email_table.html', email=email)
